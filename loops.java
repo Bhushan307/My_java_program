3270d17886b0aab90830ce8e7ffcf454e5ac0159
Bhushan Chaudhari <118585671+Bhushan307@users.noreply.github.com>
2026-03-09 21:02:18 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,10 @@
+public class loops{
+	public static void main(String [] args){
+		for(int line =2;line<=5;line++){
+			for(int i=2;i<=line;i++){
+				System.out.print(i);
+			}
+			System.out.println();
+		}
+	}
+}
